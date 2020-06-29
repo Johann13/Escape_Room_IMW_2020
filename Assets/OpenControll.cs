5c5686ab62fcf690470954c70e94bde7b8d1abc9
Johann Feser <johannfeser@gmail.com>
2020-06-29 10:36:13 +0200

    help menu

---
+++ Assets/OpenControll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpenControll : MonoBehaviour
+{
+    public Animator transition;
+    public float transitionTime = 1f;
+
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(LoadGame);
+    }
+
+    void LoadGame()
+    {
+        StartCoroutine(
+            LoadLevel(3));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        if (transition != null)
+        {
+            transition.SetTrigger("Start");
+            yield return new WaitForSeconds(transitionTime);
+        }
+
+        SceneManager.LoadScene(index);
+    }
+}
